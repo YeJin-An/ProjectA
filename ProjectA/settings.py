0dc4950dc3029db4009f9a57eb9f2e69aa979584
안예진 <ahw24033@naver.com>
2022-02-15 16:15:27 +0900

    SystemCheckError 수정

--- ProjectA/settings.py
+++ ProjectA/settings.py
@@ -146,8 +146,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # django-cors-headers
 # https://github.com/adamchainz/django-cors-headers
-
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://localhost:3000"
+]
 
 
 # django-rest-framework
 # https://www.django-rest-framework.org/#installation
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
