13ce0be9e8c33af1521145faf7cca7a9c996b7c7
안예진 <ahw24033@naver.com>
2022-02-15 18:57:10 +0900

    rest_framework added

--- ProjectA/settings.py
+++ ProjectA/settings.py
@@ -150,7 +150,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:3000"
 ]
-
+CSRF_COOKIE_NAME = "csrftoken"
 
 # django-rest-framework
 # https://www.django-rest-framework.org/#installation
