f4ca572e748636f0052e94c9406a224ee64f2106
안예진 <ahw24033@naver.com>
2022-02-25 17:11:32 +0900

    notice/form added

---
+++ notice/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from notice.models import Notice
+
+class NoticeForm(forms.ModelForm):
+  class Meta:
+    model = Notice
+    fields = "__all__"
+
+  def clen_title(self):
+    title = self.cleaned_data.get("title")
+    if title:
+      if len(title)!=5:
+        raise forms.ValidationError("5글자만!!") 
+    return title
