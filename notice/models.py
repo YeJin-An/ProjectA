9bfda097c769fe96fae7f0710d700ea3f9e6eb3e
안예진 <ahw24033@naver.com>
2022-02-25 17:07:57 +0900

    title 글자제한

--- notice/models.py
+++ notice/models.py
@@ -1,11 +1,21 @@
+from re import A
 from django.conf import settings
 from django.db import models
 from user.models import User
+from django.core.exceptions import ValidationError
+
+
+def validate_even(value):
+    if value < '5':
+      raise ValidationError(
+          _('%(value)s is not an even number'),
+            params={'value': value},
+      )
 
 class Notice(models.Model):
-
   id = models.AutoField(primary_key=True)
-  title = models.CharField(max_length=100)
+  title = models.CharField(max_length=5, validators=[validate_even])
   content = models.TextField()
   created_at = models.DateField()
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
